18f05656ea7f83e668e849498185ff32d3944432
davidosili <147920967+davidosili@users.noreply.github.com>
2025-10-14 10:37:25 +0100

    Update tracking.js

--- public/tracking.js
+++ public/tracking.js
@@ -1,7 +1,9 @@
 const BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:5000"
-    : "https://rapidroutesltd.com" || "https://rapidroutesltd.onrender.com";
+    : window.location.hostname === "rapidroutesltd.com"
+    ? "https://rapidroutesltd.com"
+    : "https://rapidroutesltd.onrender.com";
 
 
 
