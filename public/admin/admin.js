7c423d7d12932da65a502d3a44ab16bc5afe7f9b
davidosili <147920967+davidosili@users.noreply.github.com>
2026-03-14 00:54:31 +0100

    Update admin.js

--- public/admin/admin.js
+++ public/admin/admin.js
@@ -1,7 +1,7 @@
 const API_URL = 
   (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
     ? "http://localhost:5000"
-    : "https://consignment-site.vercel.app";
+    : window.location.origin;
 
 // ------------------ LOGIN ------------------
 async function loginAdmin() {
