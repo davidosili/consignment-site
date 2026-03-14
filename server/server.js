468cd7e2856d486fd434a653509839bb9a8b88f3
davidosili <147920967+davidosili@users.noreply.github.com>
2026-03-14 23:15:19 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,9 +11,9 @@ const crypto = require("crypto");
 // Route & Model Imports
 const notifyRoutes = require("./routes/notifyRoutes"); 
 const telegramNotify = require('./routes/telegramNotify');
-const Tracking = require("../models/Tracking.js"); // Ensure this path is correct relative to server.js
-const Admin = require("../models/Admin.js");       // Ensure this path is correct relative to server.js
-const TempShipment = require("../models/TempShipment"); // Ensure this path is correct relative to server.js
+const Tracking = require("./models/Tracking.js"); // Ensure this path is correct relative to server.js
+const Admin = require("./models/Admin.js");       // Ensure this path is correct relative to server.js
+const TempShipment = require("./models/TempShipment"); // Ensure this path is correct relative to server.js
 const { bot } = require('./telegramBot');
 
 const app = express();
