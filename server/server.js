7d9a372cb256fdf26a676507d0cc9cd21ce2f89f
davidosili <147920967+davidosili@users.noreply.github.com>
2026-03-14 23:22:50 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.options('*', cors());
+app.options('(.*)', cors());
 
 // ==========================================
 // 2. DATABASE CONNECTION (UPDATED FOR VERCEL)
