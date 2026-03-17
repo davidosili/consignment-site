be89b914eb206f4210a5c48f64b1c41f95320a32
davidosili <147920967+davidosili@users.noreply.github.com>
2026-03-17 23:53:21 +0100

    Enhance TempShipment schema with sender, receiver, and telegramChatId
    Added sender and receiver fields along with telegramChatId to the TempShipment schema.

--- server/models/TempShipment.js
+++ server/models/TempShipment.js
@@ -4,29 +4,38 @@ const mongoose = require("mongoose");
 const tempShipmentSchema = new mongoose.Schema(
   {
     tempId: { type: String, required: true, unique: true },
+
     sender: {
       name: String,
       email: String,
       phone: String,
       address: String,
     },
+
     receiver: {
       name: String,
       email: String,
       phone: String,
       address: String,
     },
+
     items: [
       {
         description: String,
         weight: String,
         cost: String,
-        quantity: { type: Number, default: 1 }
+        quantity: { type: Number, default: 1 },
       },
     ],
+
     status: {
       type: String,
       default: "Pending Receiver Info",
+    },
+
+    telegramChatId: {
+      type: String, // store Telegram user ID when linked
+      default: null,
     },
   },
   { timestamps: true }
