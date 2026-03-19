0d9276182f4b3287e5544060408cb7e0f1cbb6bf
davidosili <147920967+davidosili@users.noreply.github.com>
2026-03-19 17:46:30 +0100

    Update TelegramUser.js

--- server/models/TelegramUser.js
+++ server/models/TelegramUser.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
 
 const telegramUserSchema = new mongoose.Schema({
-  chatId: { type: Number, required: true, unique: true },
-  username: { type: String, default: "User" },
-  tempIds: [{ type: String }], // multiple shipments per user
-  createdAt: { type: Date, default: Date.now },
+  chatId: { 
+    type: Number, 
+    required: true, 
+    unique: true 
+  },
+  username: { 
+    type: String, 
+    default: "User" 
+  },
+  tempIds: [
+    { type: String }
+  ], // allows multiple shipments per user
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  },
 });
 
 module.exports = mongoose.model("TelegramUser", telegramUserSchema);
